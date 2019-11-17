4317fb338dd8581cb0b4e26f67feadd1e4b3f9c5
algorithm <hwangjae92@gmail.com>
2019-11-17 17:14:23 +0900

    숙제

---
+++ 191117_jaehee/homework10872/homework10872/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace homework10872
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int i = 1;
+            int n = int.Parse((Console.ReadLine()));
+
+            while(n>=i)
+            {
+               a = a * i;
+                i++;
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
